76e6b193a6a629ccca95e70442c52b1272d4b167
Bella Salter <78449253+bellasalter@users.noreply.github.com>
2025-01-31 13:25:09 -0700

    Update data_processing.R

--- data_processing.R
+++ data_processing.R
@@ -1,4 +1,14 @@
 
+# takes in a dataframe of plays in the game. type should denote the type of play. 
+get_winner <- function(game_df, tm1, tm2) {
+  goals = game_df[game_df$type == "goal",]
+  n_goals_tm1 = nrow(goals[goals$tmAbbrev == tm1,])
+  if(n_goals_tm1 > (nrow(goals) - n_goals_tm1)) {
+    return(tm1)
+  } else {
+    return(tm2)
+  }
+}
 
 get_game_ids <- function(folder_name) {
   game_ids <- c()
